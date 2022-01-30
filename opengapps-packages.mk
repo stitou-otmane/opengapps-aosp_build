808eb67479e47893afa7ed7794a229880e07425b
Otmane <stitou.otmane@gmail.com>
2022-01-30 23:36:29 +0100

    Remove CarrierServices module

--- opengapps-packages.mk
+++ opengapps-packages.mk
@@ -185,7 +185,6 @@ ifneq ($(filter super,$(TARGET_GAPPS_VARIANT)),)
 GAPPS_PRODUCT_PACKAGES += \
     Wallet \
     DMAgent \
-    CarrierServices \
     GoogleEarth \
     GCS \
     GoogleHindiIME \
